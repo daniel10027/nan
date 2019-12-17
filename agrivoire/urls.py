b230e17f3255ebee3de54bca2b88c773016aeade
Daniel vianney <danielguedegbe10027@gmail.com>
2019-12-17 11:05:10 +0000

    Update urls.py

--- agrivoire/urls.py
+++ agrivoire/urls.py
@@ -10,8 +10,10 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+`EMAIL_HOST_PASSWORD = 'optskesfjipiqeqo'
+1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    
 """
 from django.contrib.auth  import views as auth_views #vue pour les authentifications
 from django.contrib import admin
